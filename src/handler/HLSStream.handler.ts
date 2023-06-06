cdc42b4fd7104fc2f56aed600d4f5718887d047c
meisolated <isolatedbot@gmail.com>
2023-06-06 19:56:40 +0000

    next thing to be done auth

--- src/handler/HLSStream.handler.ts
+++ src/handler/HLSStream.handler.ts
@@ -4,24 +4,24 @@ import config from '../config'
 import HLSServer from '../lib/HLS'
 var lastRequest = Date.now()
 var currentStatus = "running"
+var intervalRunning = false
 export const expressHLSEndPointTrafficMiddleware = (req: Request, res: Response, next: NextFunction, chatter: EventEmitter) => {
     HLSServer(req, res, next, { hlsDir: config.HLSOutput })
-
+    lastRequest = Date.now()
     if (req.url.includes(".m3u8") || req.url.includes(".ts")) {
-        console.log("requesting HLS data")
-        lastRequest = Date.now()
         if (currentStatus == "stopped") {
             currentStatus = "running"
             chatter.emit("HLSWorker", { ask: "play" })
         }
     }
-
-    setInterval(() => {
-        if (Date.now() - lastRequest > (10000)) {
-            currentStatus = "stopped"
-            chatter.emit("HLSWorker", { ask: "stop" })
-        }
-    }, 1000)
-
+    if (!intervalRunning) {
+        setInterval(() => {
+            intervalRunning = true
+            if ((Date.now() - lastRequest) > 20000) {
+                currentStatus = "stopped"
+                chatter.emit("HLSWorker", { ask: "stop" })
+            }
+        }, 1000)
+    }
 
 }
